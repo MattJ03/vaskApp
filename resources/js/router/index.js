668032545b07680d0db7d89b1bdf91fe6c5808cf
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-07-29 18:15:36 +0100

    create and set up routes

---
+++ resources/js/router/index.js
@@ -0,0 +1,14 @@
+import { createApp } from 'vue';
+import { createRouter } from 'vue-router';
+import { createWebHistory } from 'vue-router'
+
+const routes = [
+
+];
+
+const router = createRouter({
+    routes,
+    history: createWebHistory(),
+});
+
+export default router;
