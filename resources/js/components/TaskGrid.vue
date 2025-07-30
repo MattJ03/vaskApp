d90717caef97fa3c83fde743694f9b8f94dc35ce
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-07-30 22:25:18 +0100

    TaskGrid component

--- resources/js/components/TaskGrid.vue
+++ resources/js/components/TaskGrid.vue
@@ -4,16 +4,32 @@
         <p><strong> {{ task.due_date }}</strong></p>
         <p><strong> {{ task.status}}</strong></p>
 
-
+        <button class="button-delete" @click=deleteTask>Delete</button>
     </div>
 </template>
 <script setup>
 import { ref } from 'vue';
+import axios from 'axios';
+import { useRouter} from 'vue-router';
 
-const task = defineProps({
-    type: Object,
-    required: true,
+const error = ref('');
+const router = useRouter();
+const props = defineProps({
+   task: {
+       type: Object,
+       required: true,
+   }
 });
+
+const deleteTask = async () => {
+    try {
+        await axios.delete('tasks/destroy/${props.task.id}');
+        router.back();
+        console.log('task deleted');
+    } catch(error) {
+        console.log('Task not delete', error);
+    }
+}
 </script>
 <style>
 task-container {
@@ -25,5 +41,15 @@ task-container {
     flex-direction: column;
     display: flex;
     border: 2px;
+}
+button-delete {
+     width: 50px;
+    height: 35px;
+    margin: auto;
+    border: 2px;
+    background: black;
+    border-radius: 12px;
+    flex: auto;
+    flex-direction: column;
 }
 </style>
