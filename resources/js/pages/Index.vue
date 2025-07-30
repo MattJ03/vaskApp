5a8458f6dca2e891ccfe41a168c27605019a4371
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-07-30 22:59:01 +0100

    index page

--- resources/js/pages/Index.vue
+++ resources/js/pages/Index.vue
@@ -1,13 +1,52 @@
 <template>
+    <div class="tasks">
+        <p v-if="loading"> loading tasks... </p>
+        <p v-else-if="tasks.length === 0"> No tasks </p>
+        <div v-else class="task-grid">
+            <TaskGrid
+                v-for="task in tasks"
+                :key="task.id"
+                :task="task"
+                />
 
-    <div class=""
+        </div>
+    </div>
+
 </template>
 <script setup>
 import { ref } from 'vue';
 import { reactive } from 'vue';
 import { router } from 'vue-router';
+import { onMounted } from 'vue';
+
+import TaskGrid from "../components/TaskGrid.vue";
+
+const loading = ref(true);
+const error = ref('');
+const tasks = ref('');
+
+onMounted(async () => {
+   try {
+      const res = await axios.get('tasks/index');
+      tasks.value = res.data;
+      console.log('got tasks');
+   } catch(error) {
+       console.log('couldnt get tasks', error);
+   } finally {
+       loading.value = false;
+   }
+});
 
 
 </script>
 <style>
+tasks {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+}
+tasks-grid {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+}
 </style>
